5d5076eadb19f6d0f068e3cc92c36f2c16b9e5c5
09ubberboy90 <33180714+09ubberboy90@users.noreply.github.com>
2019-02-22 11:25:07 +0000

    Added the redirection of urls toward our app

--- WadThePlanet/urls.py
+++ WadThePlanet/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from planet import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^', include('planet.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
