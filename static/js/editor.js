47cb3212d28b2b0110392a6228b7bafa4ebe11b7
Paolo Jovon <paolo.jovon@gmail.com>
2019-02-23 17:25:42 +0000

    Added Three.js from CDN

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,5 +1,5 @@
 // WadThePlanet planet viewer and editor component
 // Based on Three.js (MIT-licensed) and JQuery
 $(document).ready(function() {
-    $(document.body).append('<p>Hello world!</p>');
+    $("main").append('<p>Hello world!</p>');
 });
