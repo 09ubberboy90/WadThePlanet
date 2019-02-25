882c388548f1630340ce70025224d77fa29b6317
09ubberboy90 <33180714+09ubberboy90@users.noreply.github.com>
2019-02-25 12:21:03 +0000

    Removed mdb script and added  a navbar(WIP)

--- static/js/PageControls.js
+++ static/js/PageControls.js
@@ -1,5 +1,3 @@
-"use strict";
-
 (function ($) {
     var SCROLLING_NAVBAR_OFFSET_TOP = 50;
     $(window).on('scroll', function () {
@@ -8,8 +6,10 @@
         if ($navbar.length) {
             if ($navbar.offset().top > SCROLLING_NAVBAR_OFFSET_TOP) {
                 $('.scrolling-navbar').addClass('top-nav-collapse');
+                $('.sidebar-container').css('background-color', 'rgba(0,0,0,.3)');
             } else {
                 $('.scrolling-navbar').removeClass('top-nav-collapse');
+                $('.sidebar-container').css('background-color ', ' #1C2331;');
             }
         }
     });
