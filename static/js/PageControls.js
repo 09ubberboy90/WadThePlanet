487cdec550206ef1cfe36d444ed53acb9b4d1c76
09ubberboy90 <33180714+09ubberboy90@users.noreply.github.com>
2019-02-28 16:18:37 +0000

    Broke resizing since it breaks stuff

--- static/js/PageControls.js
+++ static/js/PageControls.js
@@ -24,21 +24,10 @@ $(function() {
 });
 
 
-$(function padder() {
-    var navheight = $('#navbar').outerHeight();
-    $('.main-screen').css('padding-top', navheight);
-    $('#sidebar').css('top', navheight);
-
-});
-
 $(window).resize(function () {
     if ($(window).width() >= 975) {
         $('.adder').addClass('collapse');
     } else if ($(window).width() < 975) {
         $('.adder').removeClass('collapse');
     }
-    padder();
-});
-$(".input_field").change(function () {
-    alert("This value changed");
 });
