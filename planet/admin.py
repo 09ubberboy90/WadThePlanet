cb77a645b70d7fb3562caff85756ca477aff3044
Trine Olsen <2420036o@student.gla.ac.uk>
2019-03-01 15:21:12 +0100

    remove one line

--- planet/admin.py
+++ planet/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from planet.models import Planet, PlanetUser, SolarSystem
-from planet.forms import CustomUserCreationForm
+#from planet.forms import CustomUserCreationForm
 
 # Register your models here.
 admin.site.register(PlanetUser)
