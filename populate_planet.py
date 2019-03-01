1e5985852dd2a66929fa7eb2b50945a9376d1f55
Trine Olsen <2420036o@student.gla.ac.uk>
2019-03-01 14:34:18 +0100

    population script done

--- populate_planet.py
+++ populate_planet.py
@@ -1,12 +1,13 @@
 import random
 import string
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WadThePlanet.settings")
 
 import django
 django.setup()
 from planet.models import PlanetUser, Planet, SolarSystem
-
+self_path = os.path.abspath(os.path.dirname(__file__))
 
 def populate():
 
@@ -15,66 +16,47 @@ def populate():
 
 
     planets1 = [ {"name": "planet1",
-                #"user": user1,
-                #"solarSystem": firstSolarSystem,
-                "texture": "/_population_pictures/texture1.jpg"},
+                  "texture": os.path.join(self_path,'media/_population_pictures/texture1.jpeg')},
 
                 {"name": "planet2",
-                #"user": user2,
-                #"solarSystem": firstSolarSystem,
-                "texture": "/_population_pictures/texture2.jpeg"},
+                 "texture": os.path.join(self_path,'media/_population_pictures/texture2.jpeg')},
     ]
 
     planets2 = [{"name": "planet3",
-                # "user": user3,
-                #"solarSystem": secondSolarSystem,
-                "texture": "/_population_pictures/texture3.jpeg"},
+                 "texture": os.path.join(self_path,'media/_population_pictures/texture3.jpeg')},
 
     ]
 
     planets3 = [{"name": "planet4",
-                #"user": user1,
-                #"solarSystem": secondSolarSystem,
-                "texture": "/_population_pictures/texture4.jpeg"},
+                 "texture": os.path.join(self_path,'media/_population_pictures/texture4.jpeg')},
     ]
 
 
 
-    SolarSystem1 = [{#"user": user1,
+    SolarSystem1 = [{
                     "name": "FirstSolarSystem",
                     "description": "Cool solarsys",
-                    "planets": {"p": planets1}
+                    "planets": planets1,
                     },
     ]
 
-    SolarSystem2 = [
-                    {#"user": user1,
+    SolarSystem2 = [{
                     "name": "SecondSolarSystem",
                     "description": "Cooler solarsys",
-                    "planets": {"p": planets2}
+                    "planets": planets2,
                     },
     ]
 
-    SolarSystem3 = [
-                    {#"user": user1,
+    SolarSystem3 = [{
                     "name": "ThirdSolarSystem",
                     "description": "Coolest solarsys",
-                    "planets": {"p": planets3}
+                    "planets": planets3,
                     },
     ]
-
-
-#    users = {"433232@hotmail.com": {"solarSys": SolarSystem1},
-#             "433232232@hotmail.com": {"solarSys": SolarSystem2},
-#             "123232@hotmail.com": {"solarSys": SolarSystem3},}
 
     users = {"geir": {"solarSys": SolarSystem1},
              "petter": {"solarSys": SolarSystem2},
              "eva": {"solarSys": SolarSystem3},}
-
-    #user1 = [{"email:" "433232@hotmail.com",  }]
-    #user2 = [{"email:" "433232232@hotmail.com", }]
-    #user3 = [{"email:" "123232@hotmail.com", }]
 
 
 
@@ -82,18 +64,15 @@ def populate():
         u = add_user(user)
         for solarsystem in solarsystems["solarSys"]:
             s = add_solarSys(u, solarsystem["name"], solarsystem["description"])
-            #for planet in solarsystem["planets"]:
-                #add_planet(planet["name"], u, s, planet["texture"])
-                #print(planet)
+            for planet in solarsystem["planets"]:
+                add_planet(planet["name"], u, s, planet["texture"])
+
 
 
 
 def add_user(username):
     email = str(username + "@hotmail.com")
     password = "".join(random.choice(string.ascii_lowercase) for i in range(10))
-    print(email)
-    print(password)
-    print(username)
     user = PlanetUser.objects.get_or_create(username=username, password=password,email=email)[0]
     user.save()
     return user
@@ -107,10 +86,12 @@ def add_planet(name, user, solarSys, texture, score=0):
 
 
 def add_solarSys(user, name, description='', score=0, views=0):
-    solarSys = SolarSystem.objects.get_or_create(user=user,name=name)[0]
+    solarSys = SolarSystem.objects.get_or_create(user=user, name=name)[0]
     solarSys.description = description
     solarSys.score = score
     solarSys.views = views
+    solarSys.save()
+    return solarSys
 
 
 #start execution here
