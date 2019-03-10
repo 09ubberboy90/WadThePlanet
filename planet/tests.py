c5f648bf2c2a6823e97858028f8b6777a9010840
Agnes Ola <2321440o@student.gla.ac.uk>
2019-03-10 18:19:36 +0000

    initial unit test

--- planet/tests.py
+++ planet/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from planet.models import Planet, PlanetUser, SolarSystem
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class UserTestCase (TestCase):
+	def setup(self):
+		PlanetUser.objects.create(username='Bob', password='Bob12345678', email='Bob@mail.com')
+		PlanetUser.objects.create(username='Anne', password='Anne12345678', email='Anne@mail.com')
+		
+	def user_account_exists(self):
+		Bob = PlanetUser.objects.get(username="Bob")
+		self.assertEqual(Bob.username, "Bob")
